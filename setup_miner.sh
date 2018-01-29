2d93cb1b08dca0d01dfa3371a7bb22fa9a6313ab
dolpen <dolpen@dolpen.net>
2018-01-29 15:11:34 +0900

    Update setup_miner.sh

--- setup_miner.sh
+++ setup_miner.sh
@@ -6,7 +6,9 @@ yum -y install epel-release git screen
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
 yum -y install jansson-devel libcurl-devel
 cd /opt
-git clone https://github.com/Libitina/cpuminer.git
+
+## install minerd / yescrypt 0.5 (bitzeny)
+git clone https://github.com/macchky/cpuminer.git
 cd cpuminer
 sh ./autogen.sh
 sh ./configure CFLAGS="-O3 -march=native -funroll-loops -fomit-frame-pointer"
