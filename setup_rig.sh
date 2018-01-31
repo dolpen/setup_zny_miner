f5fb150e15e98a16b39edb8ab435c9678bb53c93
dolpen <dolpen@dolpen.net>
2018-01-31 16:21:38 +0900

    Update setup_rig.sh

--- setup_rig.sh
+++ setup_rig.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo ${POOL_STARTUM:?'run this script with `POOL_STARTUM` env variable'} >/dev/null
 echo ${POOL_USER:?'run this script with `POOL_USER` env variable'} >/dev/null
 echo ${WORKER_NAME:?'run this script with `WORKER_NAME` env variable'} >/dev/null
 echo ${WORKER_PASSWORD:?'run this script with `WORKER_PASSWORD` env variable'} >/dev/null
@@ -29,15 +30,16 @@ wget -q https://raw.githubusercontent.com/dolpen/setup_zny_miner/develop/setup_m
 ################
 
 cd /root/
-wget -q https://raw.githubusercontent.com/dolpen/setup_zny_miner/develop/setup_invoker.sh -O - | sh
+wget -q https://raw.githubusercontent.com/dolpen/setup_zny_miner/develop/setup_invoker.sh -O - | POOL_STARTUM=$POOL_STARTUM POOL_USER=$POOL_USER POOL_WORKER=$WORKER_NAME POOL_PASSWORD=$WORKER_PASSWORD sh
+
 
 ################
-#Set variables
+#Setup cron
 ################
 
-sed -i "s/POOL_USER=\"dolpen\"/POOL_USER=\"$POOL_USER\"/" /root/invoke.sh
-sed -i "s/POOL_WORKER=\"dummy\"/POOL_WORKER=\"$WORKER_NAME\"/" /root/invoke.sh
-sed -i "s/POOL_PASSWORD=\"dummy\"/POOL_PASSWORD=\"$WORKER_PASSWORD\"/" /root/invoke.sh
+cd /root/
+wget -q https://raw.githubusercontent.com/dolpen/setup_zny_miner/develop/setup_cron.sh -O - | sh
+
 
 
 echo "# Welcome to minerd! ####################################"
@@ -66,4 +68,6 @@ for native in ${NATIVE} ; do
 done
 echo
 
-echo "try : # sh /root/invoke.sh"
+echo "## try to start mining!"
+
+sh /root/invoke.sh
