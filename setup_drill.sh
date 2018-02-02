84376cb3febc2b8b75245103393fba88292611be
dolpen <dolpen@dolpen.net>
2018-02-02 11:12:13 +0900

    Update and rename setup_miner.sh to setup_drill.sh

--- setup_drill.sh
+++ setup_drill.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # do as root
 
+if [ "$REPOS" = "" ]; then
+  echo "check REPOS variable";
+exit 1
+
 yum -y groupinstall "Development Tools"
 yum -y install epel-release git screen
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
 yum -y install jansson-devel libcurl-devel
 cd /opt
-git clone https://github.com/Libitina/cpuminer.git
+rm -rf cpuminer
+git clone $REPOS
 cd cpuminer
 sh ./autogen.sh
 sh ./configure CFLAGS="-O3 -march=native -funroll-loops -fomit-frame-pointer"
